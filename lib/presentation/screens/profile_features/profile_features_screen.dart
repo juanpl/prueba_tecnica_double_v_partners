d7daa8fa473f25d800c44c3cc44bb272ba0e5d88
Juan Pablo Lasprilla Correa <juanpablolasprillacorrea@gmail.com>
2025-03-08 14:59:09 -0500

    The 3 screens (login, profile features, sign on) were created

---
+++ lib/presentation/screens/profile_features/profile_features_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProfileFeaturesScreen extends StatelessWidget {
+  const ProfileFeaturesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/presentation/screens/sign_on/sign_on_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SignOnScreen extends StatelessWidget {
+  const SignOnScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
