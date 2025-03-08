3abc90064362c247e7612e5eb28fc910b2d41018
Juan Pablo Lasprilla Correa <juanpablolasprillacorrea@gmail.com>
2025-03-08 14:40:54 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# prueba_tecnica_double_v_partners
+
+A new Flutter project.
+# prueba_tecnica_double_v_partners
---
+++ android/app/src/main/kotlin/com/example/prueba_tecnica_double_v_partners/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.prueba_tecnica_double_v_partners
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
